9538fa14282fcec54228eb0d57a576708adf93cd
amer <amerbinazim@gmail.com>
2025-10-03 18:27:40 +0300

    added langauges

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,20 @@ import 'package:hackthon/commons/widgets/ctx_common.dart';
 import 'package:hackthon/screens/login/login_screen.dart';
 import 'package:hackthon/screens/on_boarding/on_boarding_screen.dart';
 import 'package:hackthon/theme/app_theme.dart';
+import 'package:easy_localization/easy_localization.dart';
 
-void main() {
-  runApp(MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+      supportedLocales: [Locale('ar'), Locale('en')],
+      path: 'assets/lang',
+      saveLocale: true,
+      startLocale: Locale('ar'),
+      child: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
